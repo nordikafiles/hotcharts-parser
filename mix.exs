abf847dbbfb9cc6534339608b1934f06faa72324
nordikafiles <maksiik@gmail.com>
2017-05-07 18:20:09 +0300

    deps fix

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule HotchartsParser.Mixfile do
 
   def project do
     [app: :hotcharts_parser,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
